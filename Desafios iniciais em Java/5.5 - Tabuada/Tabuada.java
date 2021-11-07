c69ca5016420f85a71ae9a0e6ad9fdd5f8362a94
Diego Jefferson <diegojfsr@gmail.com>
2021-11-07 12:07:37 -0300

    Desafios-iniciais-java

---
+++ Desafios iniciais em Java/5.5 - Tabuada/Tabuada.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Teste {
+	
+	public static void main(String[] args) throws IOException {
+    	Scanner leitor = new Scanner(System.in);
+    	int N = leitor.nextInt();
+    	System.out.println("1 x " + N + " = " + (N * 1));
+    	System.out.println("2 x " + N + " = " + (N * 2));
+    	System.out.println("3 x " + N + " = " + (         ));
+    	System.out.println("4 x " + N + " = " + (         ));
+    	System.out.println("5 x " + N + " = " + (         ));
+    	System.out.println(                                          ));
+    	System.out.println(                                          ));
+    	System.out.println(                                          ));
+    	System.out.println(                                          ));
+    	System.out.println("10 x " + N + " = " + (N * 10));
+	}
+	
+}
